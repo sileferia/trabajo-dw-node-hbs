1c02df9cf72cd7a5c14db91e28b7071fc949acb4
sileferia <silena.feria2016@gmail.com>
2020-11-16 18:17:34 -0500

    Deploy 2

--- server/public/assets/js/app.js
+++ server/public/assets/js/app.js
@@ -1,13 +1,27 @@
-// function login() {
-//     const formLogin = document.querySelector('.form-login');
-
-//     console.log(formLogin);
-// }
-
 document.querySelector('.form-login').addEventListener('submit', (e) => {
     e.preventDefault();
 
     const formLogin = document.querySelector('.form-login');
 
-    console.log(formLogin['email']);
+    const dataLogin = {
+        email: formLogin['email'].value,
+        password: formLogin['password'].value
+    }
+
+    console.log(dataLogin);
+
 });
+
+
+async function login(datalogin = null) {
+
+    if (datalogin === null) {
+        return;
+    }
+
+    const request = {
+        method: 'POST',
+        body: datalogin
+    }
+
+}
